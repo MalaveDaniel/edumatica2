8f4bd27ad79dfedbb27245de651b8d4398b12fc8
Malavedaniel04 <dmalave04@hotmail.com>
2020-09-24 01:08:08 -0400

    curso_de_laravel_2

---
+++ aprendiendo_laravel1/app/Http/Controllers/NotaController1.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nota;
+use Illuminate\Http\Request;
+use App;
+use App\Http\Controllers\HomeController;
+
+class NotaController1 extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+                //return auth()->user();
+                $usuarioEmail = auth()->user()->email;
+                $notas = Nota::where('usuario', $usuarioEmail)->paginate(5);
+                return view('notas.lista',compact('notas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('notas.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nota $nota)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nota $nota)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Nota $nota)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nota $nota)
+    {
+        //
+    }
+}
